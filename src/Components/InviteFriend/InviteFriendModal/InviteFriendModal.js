9474c6aaa5ce6f0b6ac51733038493cbcc8f3d2b
shubhamDJ19 <shubham@digitaljalebi.com>
2021-03-26 04:34:29 +0530

    added sending mail

--- src/Components/InviteFriend/InviteFriendModal/InviteFriendModal.js
+++ src/Components/InviteFriend/InviteFriendModal/InviteFriendModal.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useAlert } from 'react-alert';
 import fb from "./assets/fb.png";
 import twitter from "./assets/twitter.png";
+import axios from 'axios';
 import whatsapp from "./assets/whatsapp.png";
+import { UserContext } from '../../../Context/Auth/UserContextProvider';
+import './invite.css'
+
+const mailUrl = 'http://event.markonnect.in/inviteFriend'
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 
 export default function InviteFriendModal(props) {
     const alert = useAlert();
     const [url, setUrl] = useState(window.location.href);
     const [textArea, setTextarea] = useState({});
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState(false);
+    const [isLoading, setLoading] = useState(false)
+
+    const { user, userInfo } = useContext(UserContext)
+
 
     const copyCodeToClipboard = () => {
         textArea.disabled = false;
@@ -25,6 +42,35 @@ export default function InviteFriendModal(props) {
             });
         }
     };
+
+    const sendMail = () => {
+        return new Promise((res, rej) => {
+            setError(false);
+            setLoading(true)
+            if (email === "") {
+                setError(true)
+                setLoading(false)
+                return
+            }
+            if (!validateEmail(email)) {
+                setError(true)
+                setLoading(false)
+            }
+            axios.post(mailUrl,
+                {
+                    name: user.displayName,
+                    senderMail: email.toLowerCase(),
+                    email: userInfo.email,
+                }).then(res => {
+                    alert.success("Mail sent successfully!!")
+                    setLoading(false)
+                }).catch(err => {
+                    setError(true)
+                    alert.error("Failed: Mail not sent.")
+                    setLoading(false)
+                })
+        })
+    }
 
     return (
         <div className="modalBox modalBox--small active blackTint modalBoxAppearAnim">
@@ -97,10 +143,33 @@ export default function InviteFriendModal(props) {
                                 type="text"
                                 className="form-control"
                                 placeholder="Enter email ID"
+                                value={email}
+                                onChange={(e) => {
+                                    e.preventDefault()
+                                    setEmail(e.target.value)
+                                }}
                             />
                         </div>
                     </div>
-                    <button className="btn btn-secondary">Send Mail</button>
+
+                    {
+                        error &&
+                        <>
+                            <div className="form-group mg-b30">
+                                <span style={{ color: 'red', marginBottom: '1rem' }}>*Enter a valid email.</span>
+                            </div>
+                        </>
+                    }
+                    <button className="btn btn-secondary" onClick={sendMail} disabled={isLoading ? true : false}>
+                        {
+                            isLoading ?
+                                (
+                                    <>
+                                        <img id="btnLoader" src="/assets/images/loader.gif" alt="loading" />
+                                    </>
+                                ) : ('Send Mail')
+                        }
+                    </button>
                 </div>
             </div>
         </div>
