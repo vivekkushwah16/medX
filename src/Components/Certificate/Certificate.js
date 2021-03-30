bd3a818163b328fc5caabb72365d5a52fad9f137
shubhamDJ19 <shubham@digitaljalebi.com>
2021-03-30 19:23:01 +0530

    added certificate

---
+++ src/Components/Certificate/Certificate.js
@@ -0,0 +1,31 @@
+import React, { createRef, useContext } from 'react'
+import { UserContext } from '../../Context/Auth/UserContextProvider'
+import ButtonWithLoader from '../ButtonWithLoader/ButtonWithLoader';
+import './Certificate.css'
+import html2canvas from 'html2canvas'
+import canvasToImage from 'canvas-to-image'
+
+export default function Certificate() {
+    const { user } = useContext(UserContext);
+    const certificatBody = createRef(null)
+
+    const downloadCert = async () => {
+        if (certificatBody.current) {
+            const canvas = await html2canvas(certificatBody.current)
+            canvasToImage(canvas, {
+                name: 'certificate',
+                type: 'jpg',
+                quality: 0.7
+            });
+        }
+    }
+
+    return (
+        <div className="certificateContainer" ref={certificatBody}>
+            <img src="/assets/images/certi.jpg" width="100%" height="auto" className="certificate" />
+            <span class="your_name" id="your_name">{user.displayName}</span>
+            <ButtonWithLoader className="btn btn-secondary certDownload" name={'Download'} handleClick={downloadCert} />
+        </div>
+
+    )
+}
