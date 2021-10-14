afc47b4f5d31eca141d45f63b5807cf747998179
shubhamDJ19 <shubham@digitaljalebi.com>
2021-10-14 17:56:42 +0530

    included cloud function for recommendation

--- src/AppConstants/CloudFunctionName.js
+++ src/AppConstants/CloudFunctionName.js
@@ -6,5 +6,6 @@ export const RETRY_OTP_CLOUDFUNCTION = "resendOTP";
 export const UPDATE_MOBILENUMBER_CLOUDFUNCTION = "updateMobileNumber";
 export const GET_LOCATION_DATA = "getLocationData";
 export const SEARCH_CLOUD_NAME = "search";
+export const GET_SIMPLE_RECOMMENDATION = "getSimpleRecommendation"
 
 export const USER_TOKEN_UPDATE = "userTokenUpdate";
