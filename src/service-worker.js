c447073fc0c2af4f2204685738694a7c1d4ff1e1
shubhamDJ19 <shubham@digitaljalebi.com>
2021-12-04 11:25:53 +0530

    updated version

--- src/service-worker.js
+++ src/service-worker.js
@@ -13,7 +13,7 @@ import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
 
-const currentVersion = "V0.2.8.2";
+const currentVersion = "V0.2.8.3";
 console.log(currentVersion);
 clientsClaim();
 
